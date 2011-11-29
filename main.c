15ba485a52a509a531e1cb3f92ad50144ce7b68b
Etienne Girondel <lenaing@gmail.com>
2011-11-30 00:37:57 +0100

    Major rewrite to comply to the new libonsen plugin architecture

--- main.c
+++ main.c
@@ -34,7 +34,10 @@
 #include "config.h"
 #include "globals.h"
 
+#include <math.h>
+#include <libonsen/plugin.h>
 #include <libonsen/archive_plugin.h>
+#include <libonsen/string_utils.h>
 #include <libonsen/file_utils.h>
 #include <libonsen/onsen.h>
 
@@ -59,28 +62,199 @@ void stop()
     fprintf(stdout, "            @(⌒_⌒)@            \n\n");
 }
 
+double round(double d)
+{
+  return floor(d + 0.5);
+}
+
+void progress(int total, int current, void *data) {
+
+   
+    int progressBarWidth;
+    float f;
+    int iPos;
+    int iProgressChars;
+
+    (void) data;
+    iPos = 0;
+    progressBarWidth=40;
+
+    f = (float)current / (float)total;
+    
+    iProgressChars = round(f * progressBarWidth);
+    
+    printf("[");
+   
+    for ( ; iPos < iProgressChars; iPos++) {
+        printf("*");
+    }
+   
+    for ( ; iPos < progressBarWidth; iPos++) {
+        printf("-");
+    }
+   
+    printf("] %3.0f%%\r", f*100);
+    if (total == current) {
+        printf("\n");
+    }
+    fflush(stdout);
+}
+
+
+
 int main(/*int argc, char *argv[]*/)
 {
-    OnsenArchivePlugin_t *pPlugin; 
+    OnsenPlugin_t *pPlugin;
+    OnsenArchivePlugin_t *pInstance;
+    OnsenArchiveInfo_t *pInfo;
+    OnsenArchiveEntry_t *pEntry;
+
     FILE *pFile;
+    int rc;
+    int i, j;
+    int writeToDisk = 0;
+
+    char *szPlugin = "plugins/dummy.so";
+    char *szSrcFile = "/home/lenain/dummy.bod";
+    char *szPath = "/home/lenain/tmp/";
+    char *szPath2 = "/home/lenain/tmp2/";
+    char *szDestFilename;
+    char *szOutputDir;
 
     start();
 
-    pFile = onsen_open_file("/home/lenain/Public/dummy.blah","rb");
+    /* Load plugin */
+    pPlugin = onsen_new_plugin();
+    rc = onsen_load_plugin(pPlugin, szPlugin);
+    if (rc != 0) {
+        /* Failed to load plugin. Abort! ABORT! */
+        onsen_unload_plugin(pPlugin);
+        onsen_free_plugin(pPlugin);
+        onsen_err_critical("Failed to load plugin file '%s'. Aborting.",
+                           szPlugin);
+    }
 
+    pFile = onsen_open_file(szSrcFile,"rb");
+    if (NULL == pFile) {
+        /* Failed to open given file. */
+        onsen_unload_plugin(pPlugin);
+        onsen_free_plugin(pPlugin);
+        onsen_err_critical("Failed to open file '%s'. Aborting.", szSrcFile);
+    }
 
-    pPlugin = onsen_new_archive_plugin();
-    onsen_load_archive_plugin(pPlugin,"plugins/dummy.so");
+    rc = pPlugin->isFileSupported(szSrcFile, 0, pFile);
+    if (0 == rc) {
+        onsen_out_ok("File '%s' is supported!", szSrcFile);
+    } else {
+        /* File is unsupported. */
+        onsen_unload_plugin(pPlugin);
+        onsen_free_plugin(pPlugin);
+        onsen_err_critical("File '%s' is NOT supported. Aborting", szSrcFile);
+    }
 
+    /* onsen_get_archive_info example ----------------------------------------*/
+    onsen_err_warning("");
+    onsen_err_warning("Example of onsen_get_archive_info usage :");
+    onsen_err_warning("");
 
-    pPlugin->openArchive(pPlugin, pFile);
+    /* Retrieve archive info */
+    pInfo = onsen_new_archive_info();
+    pInstance = pPlugin->pInstance;
+    pInstance->getArchiveInfo(szSrcFile, 0, 0, pInfo);
 
-    pPlugin->extractAllArchive(pPlugin, "/home/lenain/tmp/");
+    /* Print archive info */
+    onsen_out_ok("Archive size = %ld", pInfo->lArchiveFileSize);
+    onsen_out_ok("Archive entries count = %d", pInfo->iArchiveEntriesCount);
+    onsen_out_ok("");
 
-    pPlugin->closeArchive(pPlugin);
+    /* For each file in archive, print info */
+    for (i = 0; i < pInfo->iArchiveEntriesCount; i++) {
 
-    onsen_unload_archive_plugin(pPlugin);
-    onsen_free_archive_plugin(pPlugin);
+        if (NULL == pInfo->a_pArchiveEntries) {
+            /* No archive entry defined. Stop here.*/
+            break;
+        }
+
+        pEntry = pInfo->a_pArchiveEntries[i];
+        if (NULL == pEntry) {
+            /* Invalid entry, check next. */
+            continue;
+        }
+
+        onsen_out_ok("File '%s' :", pEntry->szFilename);
+        onsen_out_ok("\t Size : %d", pEntry->iSize);
+        onsen_out_ok("\t Commpressed size : %d", pEntry->iCompressedSize);
+        onsen_out_ok("\t Encrypted : %s", (pEntry->bEncrypted) ? "yes" : "no");
+        onsen_out_ok("\t Compressed : %s", (pEntry->bCompressed) ? "yes" : "no");
+        onsen_out_ok("\t Additional fields :");
+        for (j = 0; j < pEntry->iAddlFdsCount; j++) {
+            if (NULL != pEntry->a_szAddlFds[j]) {
+                onsen_out_ok("\t\t %s : %s", pInfo->a_pArchiveEntries[pInfo->iArchiveEntriesCount]->a_szAddlFds[j], pEntry->a_szAddlFds[j]);
+            }
+        }
+
+        if (writeToDisk) {
+            /* Correct filename for an UNIX system. */
+            szDestFilename = onsen_build_filename(szPath, pEntry->szFilename);
+            onsen_str_chr_replace(szDestFilename,'\\', '/');
+
+            /* Build directory tree. */
+            szOutputDir = onsen_basedir(szDestFilename);
+            onsen_mkdir(szOutputDir);
+
+            /* Write file to disk. */
+            pInstance->writeFile(szSrcFile, 0, pEntry->iOffset, pEntry->iSize, szDestFilename, 0, progress, NULL);
+
+            /* Free path and filename */
+            onsen_free(szOutputDir);
+            onsen_free(szDestFilename);
+        }
+    }
+
+    onsen_free_archive_info(pInfo);
+
+    /* onsen_get_file_info example -------------------------------------------*/
+    onsen_err_warning("");
+    onsen_err_warning("Example of onsen_get_file_info usage :");
+    onsen_err_warning("");
+
+    pEntry = onsen_new_archive_entry();
+    rc = pInstance->getFileInfo(szSrcFile, 0, "blah0", 0, pEntry);
+
+    if (0 == rc) {
+        onsen_out_ok("File '%s' :", pEntry->szFilename);
+        onsen_out_ok("\t Size : %d", pEntry->iSize);
+        onsen_out_ok("\t Commpressed size : %d", pEntry->iCompressedSize);
+        onsen_out_ok("\t Encrypted : %s", (pEntry->bEncrypted) ? "yes" : "no");
+        onsen_out_ok("\t Compressed : %s", (pEntry->bCompressed) ? "yes" : "no");
+        onsen_out_ok("\t Additional fields :");
+        for (j = 0; j < pEntry->iAddlFdsCount; j++) {
+            if (NULL != pEntry->a_szAddlFds[j]) {
+                onsen_out_ok("\t\t %s : %s", "CRC", pEntry->a_szAddlFds[j]);
+            }
+        }
+
+        if (writeToDisk) {
+            /* Correct filename for an UNIX system. */
+            szDestFilename = onsen_build_filename(szPath2, pEntry->szFilename);
+            onsen_str_chr_replace(szDestFilename,'\\', '/');
+
+            /* Build directory tree. */
+            szOutputDir = onsen_basedir(szDestFilename);
+            onsen_mkdir(szOutputDir);
+
+            /* Write file to disk. */
+            pInstance->writeFile(szSrcFile, 0, pEntry->iOffset, pEntry->iSize, szDestFilename, 0, progress, NULL);
+
+            /* Free path and filename */
+            onsen_free(szOutputDir);
+            onsen_free(szDestFilename);
+        }
+    }
+    
+    onsen_free_archive_entry(pEntry);
+    onsen_unload_plugin(pPlugin);
+    onsen_free_plugin(pPlugin);
 
     onsen_close_file(pFile);
     stop();
