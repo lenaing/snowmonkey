ac3dd08cc7fdf777a3423bab19d197a90438830f
Etienne Girondel <lenaing@gmail.com>
2012-03-25 20:18:56 +0200

    Removed unused function

--- src/string_utils.c
+++ src/string_utils.c
@@ -33,17 +33,6 @@
  */
 #include "string_utils.h"
 
-char *
-concat(char *src1, char *src2)
-{
-    char *tmp;
-
-    tmp = calloc(strlen(src1) + strlen(src2) + 1, sizeof(char));
-    strcat(tmp, src1);
-    strcat(tmp, src2);
-    return tmp;
-}
-
 int
 is_slashed(char *str)
 {
